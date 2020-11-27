49cb62467a2cdf0471a2a3088079de09aefd5d5c
Sanjay <sanjay.kumar@daimler.com>
2020-11-27 15:48:51 +0530

    problem #230, solution 2

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ dailycodingproblem
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
-|230|[230](https://www.dailycodingproblem.com) | [swift](./source/230.swift)|Medium|
+|[230](https://www.dailycodingproblem.com)|[Super Egg drop ](https://leetcode.com/problems/super-egg-drop/) | [swift](./source/230.swift)|Medium|
--- source/230.swift
+++ source/230.swift
@@ -8,7 +8,7 @@ For example, if N = 1 and k = 5, we will need to try dropping the egg at every f
 
 */
 
-
+solution 1 : DP 
 func superEggDrop(_ K: Int, _ N: Int) -> Int {
     if N == 0 || N == 1 {
         return N
@@ -28,4 +28,44 @@ func superEggDrop(_ K: Int, _ N: Int) -> Int {
         }
     }
     return N
+}
+
+solution 2 : Binary search 
+
+var n = 0
+var dp: [[Int]]!
+func superEggDrop(_ K: Int, _ N: Int) -> Int {
+    n = N
+    dp = [[Int]](repeating: [Int](repeating: -1, count: K+1), count: N+1)
+    let res = dfs(N, K)
+    return res
+}
+
+func dfs(_ floor: Int, _ egg: Int) -> Int {
+    guard egg > 1 else {
+        return floor
+    }
+    guard floor > 1 else {
+        return 1
+    }
+    guard dp[floor][egg] == -1 else {
+        return dp[floor][egg]
+    }
+    var res = Int.max
+    var low = 1, high = floor
+    while low <= high {
+        let mid = low + (high-low)/2
+        let left = dfs(mid-1, egg-1)
+        let right = dfs(floor-mid, egg)
+        let cur = 1 + max(left, right)
+        if left > right {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+        res = min(res, cur)
+    }
+    
+    dp[floor][egg] = res
+    return res
 }
