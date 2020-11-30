9a1c1aa58913c3e9b83f66f11de80c84f9e3dcd3
Sanjay <sanjaykumarmurthy@gmail.com>
2020-11-30 11:36:00 +0530

    problem #233

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ dailycodingproblem
 |---| ----- | -------- | ---------- |
 |[230](https://www.dailycodingproblem.com)|[Super Egg drop ](https://leetcode.com/problems/super-egg-drop/) | [swift](./source/230.swift)|Medium|
 |[231](https://www.dailycodingproblem.com)|[Reorganize String ](https://leetcode.com/problems/reorganize-string/) | [swift](./source/231.swift)|Medium|
+|[233](https://www.dailycodingproblem.com)|[Fibonacci sequence ](https://leetcode.com/problems/fibonacci-number/) | [swift](./source/233.swift)|Medium|
--- source/233.swift
+++ source/233.swift
@@ -4,5 +4,21 @@ This problem was asked by Apple.
 Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space.
 */
 
-
 import Foundation
+
+let sqrt_of_5 = sqrt(Double(5))
+let Φ: Double = ((1 + sqrt(5.0)) / 2.0)
+
+func fib(_ index: Int) -> Int {
+  
+  guard index >= 2 else {
+    return index
+  }
+  
+  let doubleIndex = Double(index)
+  
+  // F(n) = (Phi^n - (-Phi)^-n) / √5
+  let numerator = pow(Φ, doubleIndex) - pow((-1.0 * Φ),(-1.0 * doubleIndex))
+  return Int(numerator / sqrt_of_5)
+}
+
